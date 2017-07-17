6fcd99aa9441cffd30a33c7818147a6face86121
huanghuiqi <huiqihuang@126.com>
2017-07-17 18:03:17 +0800

    后台管理入口

--- admin.php
+++ admin.php
@@ -18,6 +18,10 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
+define('DB_FIELD_CACHE',false);
+define('HTML_CACHE_ON',false);//www.phpernote.com/
+define('HTML_PATH', './');
+
 $_GET['m'] = (!isset($_GET['m']) || !$_GET['m'] ) ? 'admin' : $_GET['m'];
 $_GET['c'] = (!isset($_GET['c']) || !$_GET['c'] ) ? 'index' : $_GET['c'];
 $_GET['a'] = (!isset($_GET['a']) || !$_GET['a'] ) ? 'index' : $_GET['a'];
