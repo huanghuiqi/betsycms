1a030c9999b3e4eef8f204e248296ea08f6c024d
huanghuiqi <huiqihuang@126.com>
2017-07-17 18:02:31 +0800

    公共库

--- Public/js/admin/common.js
+++ Public/js/admin/common.js
@@ -2,7 +2,7 @@
 
 /*
 * 添加按钮操作
-* */
+**/
 $('#button-add').click(function(){
     var url = SCOPE.add_url;
     console.log(url);
@@ -14,6 +14,22 @@ $('#button-add').click(function(){
 * */
 $('#singcms-button-submit').on('click',function(){
     var data = $('#singcms-form').serializeArray();
+    //console.log(data);
+    var postData = {};
+    $(data).each(function(i){
+        postData[this.name] = this.value;
+    })
+    //console.log(postData);
 
+    var url = SCOPE.save_url;
+    var jump_url = SCOPE.jump_url;
+    //将获取到的数据post到服务器
+    $.post(url,postData,function(res){
+        if(res.status == 1){
+            return dialog.success(res.message,jump_url);
+        }else if(res.stutus == 0){
+            return dialog.error(res.message);
+        }
+    },'JSON');
 });
 
