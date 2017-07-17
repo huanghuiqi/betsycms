ab8d592d797cf7f6377fcd0c475286f0d4748629
huanghuiqi <huiqihuang@126.com>
2017-07-17 18:03:34 +0800

    前台入口

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
+define('DB_FIELD_CACHE',false);
+define('HTML_CACHE_ON',false);//www.phpernote.com/
+define('HTML_PATH', './');
 
 // 定义应用目录
 define('APP_PATH', './Application/');
