aab26e65e6c93752e8784e2c0d382e72da7ab05f
huanghuiqi <huiqihuang@126.com>
2017-07-20 17:52:39 +0800

    公共库

--- Public/js/admin/common.js
+++ Public/js/admin/common.js
@@ -59,5 +59,66 @@ $('#singcms-button-submit').on('click',function(){
         }else if(res.status == 0){
             dialog.error(res.message);
         }
-    },'JSON')
+    },'JSON');
+})
+
+/*
+ * 删除菜单名操作
+ * */
+$('.singcms-table #singcms-delete').on('click',function(){
+    var id = $(this).attr('attr-id');
+    var a = $(this).attr('attr-a');  //menu
+    var message = $(this).attr('attr-message');
+    var url = SCOPE.set_status_url;
+
+    pdata = {};
+    pdata['id'] = id;
+    pdata['status'] = -1;
+
+    layer.open({
+        type : 0,
+        title : '是否提交？',
+        btn: ['yes', 'no'],
+        icon : 3,
+        closeBtn : 2,
+        content: "是否确定"+message,
+        scrollbar: true,
+        yes: function(){
+            // 执行相关跳转
+            todelete(url, pdata);
+        },
+
+    });
+});
+
+//删除
+function todelete (url,data) {
+    $.post(url,data,function (res) {
+        if (res.status == 1) {
+            return dialog.success(res.message, '')
+        }
+        else {
+            return dialog.error(res.message);
+        }
+    }, 'JSON');
+}
+
+//更新排序
+$('#button-listorder').on('click',function(){
+    var data = $('#singcms-listorder').serializeArray();
+    var postData = {};
+    $(data).each(function(i){
+        postData[this.name] = this.value;
+    })
+
+    var url = SCOPE.listorder_url;
+    //将获取到的数据post到服务器
+    $.post(url,postData,function(res){
+        if(res.status == 1){
+            console.log(res)
+            return dialog.success(res.message,'');
+        }else if(res.stutus == 0){
+            return dialog.error(res.message);
+        }
+    },'JSON');
 })
